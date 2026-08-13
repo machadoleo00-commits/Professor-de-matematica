78bb18dfc42c4634ff2339a6c44729ddc47ad9b9
machadoleo00-commits <machado.leo00@gmail.com>
2026-08-13 12:10:44 -0300

    feat: adiciona escolha entre soma, subtração, multiplicação e divisão

--- professor.py
+++ professor.py
@@ -1,4 +1,10 @@
 import random
+print("Escolha a operação:")
+print("1 - Soma")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+operacao = input("Digite o número da operação: ")
 maximo =  int(input('escolha o numero maximo da tabuada desejada: '))
 quantidade =  int(input('escolha o numero de perguntas que deseja: '))
 acertos = 0
@@ -6,16 +12,29 @@ gabarito = []
 for i in range(quantidade):
     n1 = random.randint(1,maximo)
     n2 = random.randint(1,maximo)
-    resposta_certa = n1 * n2
-    resposta = int(input(f'\n{i+1}: {n1} x {n2} = '))
+    if operacao == "1":
+        resposta_certa = n1 + n2
+        simbolo = "+"
+    if operacao == "2":
+        resposta_certa = n1 - n2
+        simbolo = "-"
+    if operacao == "3":
+        resposta_certa = n1 * n2
+        simbolo = "x"
+    if operacao == "4":
+        n1 = n1 * n2
+        resposta_certa = n1 / n2
+        simbolo = "/"
+    
+    resposta = int(input(f'\n{i+1}: {n1} {simbolo} {n2} = '))
     if resposta == resposta_certa:
         print("\nCerto!")
         acertos += 1
     else:
-        print(f"\nerrado! a resposta certa era {resposta_certa}.")
-    gabarito.append((n1, n2, resposta_certa))
-print(f"\nvoce acertou {acertos} de {quantidade} de perguntas.")
-print(f"\nvocê tirou {acertos / quantidade * 100}")
+        print(f"\nErrado! A resposta certa era {resposta_certa}.")
+    gabarito.append((n1, simbolo, n2, resposta_certa))
+print(f"\nVocê acertou {acertos} de {quantidade} de perguntas.")
+print(f"\nVocê tirou {acertos / quantidade * 100}")
 print(f"\n--- GABARITO ---")
-for i, (numero1, numero2, resultado) in enumerate (gabarito, start=1):
-    print(f"pergunta {i} : {numero1} x {numero2} = {resultado}")
+for i, (numero1, simb, numero2, resultado) in enumerate (gabarito, start=1):
+    print(f"Pergunta {i} : {numero1} {simb} {numero2} = {resultado}")
